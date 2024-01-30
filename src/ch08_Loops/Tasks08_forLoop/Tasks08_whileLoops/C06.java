fb9672c8e0d93b7612ad17eff26ae1137e8722fe
zerrinizgu <135869206+zerrinizgu@users.noreply.github.com>
2024-01-30 16:17:09 +0100

    Delete src directory

--- src/ch08_Loops/Tasks08_forLoop/Tasks08_whileLoops/C06.java
+++
@@ -1,9 +0,0 @@
-package ch08_Loops.Tasks08_forLoop.Tasks08_whileLoops;
-
-public class C06 {
-    public static void main(String[] args) {
-
-
-
-    }
-}
--- src/ch10_methodCreation/C08_AtmProject.java
+++
@@ -1,8 +0,0 @@
-package ch10_methodCreation;
-
-import java.util.Scanner;
-
-public class C08_AtmProject {
-    //TODO buradaki methodlari ayri bir class alin, switch case leri ayri bir class a alin ve buradan run edin
-
-}
